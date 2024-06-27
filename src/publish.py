edd560bd09eb3a1967261c4ecee37214b2e7e7d5
yosukemaeda <yoke1390@outlook.jp>
2024-06-27 10:24:06 +0900

    台詞の処理をリファクタリング

--- src/publish.py
+++ src/publish.py
@@ -1,6 +1,7 @@
 import re
 
 LINE_MAX_TEXT = 38
+CHARACTER_LIST = ["青年", "少女", "鬼神", "導師"]
 
 input_path = "index.md"
 output_path = "out/out.md"
@@ -35,6 +36,8 @@ def join_files():
                 part_lines = f.readlines()
         # 場面の内容を追加
         for part_line in part_lines:
+            # 改行と角括弧を削除
+            # 改行の処理はparse()で行う
             part_line = re.sub(r"[\n\[\]]", "", part_line)
             result.append(part_line)
 
@@ -60,38 +63,45 @@ def indent(text, prefix):
     return result
 
 
-def parse(text):
-    """セリフとト書き、シーン名を整形"""
-
-    if len(text) == 0:
-        return "　\n\n"
-
-    if text[:6] == "scene:":
-        text = text[6:]
-        text = re.sub(r"([a-zA-Z0-9]+)",
-                      r'<span class="tcy">\1</span>',
-                      text)
-        return text + "\n　\n\n"
-
-    # 縦中横の処理
-    text = re.sub(r"([a-zA-Z0-9]+)",
+def tcy(text):
+    """縦中横の処理"""
+    return re.sub(r"([a-zA-Z0-9]+)",
                   r'<span class="tcy">\1</span>',
                   text)
 
-    if text[:3] == "青年：":
-        return indent(text[3:], "青年　　　") + "\n\n"
 
-    if text[:3] == "少女：":
-        return indent(text[3:], "少女　　　") + "\n\n"
+def indent_character_word(text):
+    """キャラクターの台詞のインデントをつける"""
+    max_indent = max([len(character) for character in CHARACTER_LIST]) + 3
+    for character in CHARACTER_LIST:
+        if text[:len(character) + 1] == character + "：":
+            return indent(text[len(character) + 1:], character + "　" * (max_indent - len(character)))
+    return None
 
-    if text[:3] == "鬼神: ":
-        return indent(text[3:], "鬼神　　　") + "\n\n"
 
-    if text[:3] == "導師：":
-        return indent(text[3:], "導師　　　") + "\n\n"
+def parse(text):
+    """セリフとト書き、シーン名を整形"""
 
+    # 空行の処理
+    if len(text) == 0:
+        return "　\n\n"
+
+    # シーン名の処理
+    if text[:6] == "scene:":
+        text = text[6:]
+        text = tcy(text)
+        return text + "\n　\n\n"
+
+    # 縦中横の処理
+    text = tcy(text)
+
+    # セリフの処理
+    words = indent_character_word(text)
+    if words:
+        return words + "\n\n"
+
+    # ト書きの処理
     return indent(text, "　　　") + "\n\n"
-
 
 
 def convert(joined_lines, output_path):
